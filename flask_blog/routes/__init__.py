ac2570953b0e650139686a7578507a0ad0f212eb
kalenshi <kalenshi@gmail.com>
2022-04-03 09:29:46 -0300

    Created a login/logout system

--- flask_blog/routes/__init__.py
+++ flask_blog/routes/__init__.py
@@ -3,3 +3,5 @@ from .home import home
 from .login import login
 from .page_not_found import page_not_found
 from .register import register
+from .logout import logout
+from .account import account
--- flask_blog/routes/login.py
+++ flask_blog/routes/login.py
@@ -1,24 +1,24 @@
-from flask import render_template, flash, redirect, url_for, session
-from flask_blog import app
+from flask import render_template, flash, redirect, url_for, session, request
+from flask_blog import app, bcrypt
 from flask_blog.forms import LoginForm
+from flask_blog.models import User
+from flask_login import login_user, current_user
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = LoginForm()
-    form_email = ""
-    user = session.get("user")
-    if user:
-        form_email = user.get("email")
-
     if form.validate_on_submit():
-        email = form.email.data
-        password = form.password.data
-        remember = form.remember.data
-        session["email"] = email
-        if email == "kalenshi@gmail.com" and password == "123":
-            flash(message="You have been logged in!", category="success")
-            return redirect(location=url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get("next")
+            if next_page and next_page.startswith("/"):
+                next_page = next_page[1:]
+            return redirect(url_for(next_page)) if next_page else redirect(url_for("home"))
+
         else:
             flash(message="Invalid credentials!", category="danger")
-    return render_template("login.html", form=form, form_email=form_email)
+    return render_template("login.html", form=form)
