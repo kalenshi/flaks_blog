e325c11263a78b34a0a8f6f4c705e01d992621a3
kalenshi <kalenshi@gmail.com>
2022-04-04 09:41:41 -0300

    first pass at creating posts

---
+++ flask_blog/forms/create_post_form.py
@@ -0,0 +1,13 @@
+from flask_wtf.form import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+
+
+class CreatePostForm(FlaskForm):
+    """
+    This is the form for users to create new posts
+    """
+    title = StringField(label="Title", validators=[DataRequired(), ])
+    content = TextAreaField(label="Body", validators=[DataRequired(), ])
+
+    submit = SubmitField(label="Post")
--- flask_blog/routes/__init__.py
+++ flask_blog/routes/__init__.py
@@ -5,3 +5,4 @@ from .page_not_found import page_not_found
 from .register import register
 from .logout import logout
 from .account import account
+from .new_posts import new_post
