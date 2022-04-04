a050694288f39e9d86154d261fa4656eca471662
kalenshi <kalenshi@gmail.com>
2022-04-04 10:03:29 -0300

    Updated repo by merging in forgotten branch `feature/login_logOut`

--- flask_blog/forms/__init__.py
+++ flask_blog/forms/__init__.py
@@ -1,6 +1,3 @@
 from .login_form import LoginForm
 from .register_form import RegistrationForm
-<<<<<<< HEAD
-=======
 from .update_account_form import UpdateAccountForm
->>>>>>> completed work on the accounts page. users able to upload profile picture
